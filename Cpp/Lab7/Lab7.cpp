9a0b975eb4d7696c034217dc475d959f2d430328
Xeltalliv <vadim@vvv.kiev.ua>
2021-12-23 00:00:14 +0200

    Use constant

--- Cpp/Lab7/Lab7.cpp
+++ Cpp/Lab7/Lab7.cpp
@@ -7,10 +7,12 @@ void fillArrayC(char[], char[], char[]);
 void printArray(const char[], char[]);
 double findAverage(char[]);
 
+int const n = 10;
+
 int main() {
-    char arrayA[10];
-    char arrayB[10];
-    char arrayC[10];
+    char arrayA[n];
+    char arrayB[n];
+    char arrayC[n];
     initArrays(arrayA, arrayB);
     fillArrayC(arrayA, arrayB, arrayC);
     double avrg = findAverage(arrayC);
@@ -21,16 +23,16 @@ int main() {
 }
 
 void initArrays(char arrayA[], char arrayB[]) {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < n; i++) {
         arrayA[i] = 45 + 2 * i;
         arrayB[i] = 61 - 2 * i;
     }
 }
 
 void fillArrayC(char arrayA[], char arrayB[], char arrayC[]) {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < n; i++) {
         arrayC[i] = 0;
-        for (int j = 0; i < 10 && arrayC[i] == 0; j++) {
+        for (int j = 0; i < n && arrayC[i] == 0; j++) {
             if (arrayA[i] == arrayB[j]) arrayC[i] = arrayA[i];
         }
     }
@@ -39,7 +41,7 @@ void fillArrayC(char arrayA[], char arrayB[], char arrayC[]) {
 double findAverage(char arr[]) {
     double total = 0;
     double amount = 0;
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < n; i++) {
         if (int(arr[i]) > 55) {
             total += double(arr[i]);
             amount++;
@@ -50,7 +52,7 @@ double findAverage(char arr[]) {
 
 void printArray(const char msg[], char arr[]) {
     std::cout << msg << ": ";
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < n; i++) {
         std::cout << arr[i] << " ";
     }
     std::cout << "\n";
